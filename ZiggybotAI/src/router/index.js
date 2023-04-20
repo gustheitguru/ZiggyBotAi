d413981714d27aebca186e528c6857985914b675
Gus <gus@theyogidude.com>
2023-04-19 23:14:06 -0700

    commit this working app

--- ZiggybotAI/src/router/index.js
+++ ZiggybotAI/src/router/index.js
@@ -1,23 +1,71 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'LandingPage',
+      component: () => import('../views/LandingPage.vue'),
+      meta: {
+        requiresAuth: false,
+      }
+    }, 
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: () => import('../views/Settings.vue'),
+      meta: {
+        requiresAuth: true,
+      }
+    },    {
+      path: '/mobile',
+      name: 'MobileChat',
+      component: () => import('../views/MobileChat.vue'),
+      meta: {
+        requiresAuth: false,
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/chat',
+      name: 'ZiggyBotAI',
+      component: () => import('../views/ChatBox.vue'),
+      meta: {
+        requiresAuth: true,
+      }
     }
   ]
-})
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+        (user) => {
+          removeListener();
+          resolve(user);
+        },
+      reject
+    )
+  });
+
+
+};
+
+router.beforeEach(async (to, from, next)=>{
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser) {
+      next();
+    } else {
+      alert("you dont have access!");
+      next("/");
+    }
+
+  } else {
+    next();
+  }
+});
 
 export default router
